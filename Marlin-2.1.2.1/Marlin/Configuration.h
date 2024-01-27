6e2fb7f075bcb9e8134f333997d2e04ec5e84215
Anuar R. Giménez El Amrani <anuargimenez@usal.es>
2024-01-27 16:22:13 +0100

    Config changes

--- Marlin-2.1.2.1/Marlin/Configuration.h
+++ Marlin-2.1.2.1/Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(MEW Pellet Extruder Controller, Anuar R. Gimenez)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(MEW Pellet Extruder Controller, Anuar R. Gimenez El Amrani)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
